30bdc12fd4a8467c8c84d64ddca3daefe89e66a7
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-11-19 20:37:03 +0100

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navigation" :class="{ show: isShowing }">
+    <div class="show-navigation" @click="showNav">
+      <div :class="{ show_bar: !isShowing, close_bar: isShowing }"></div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const isShowing = ref(false);
+
+    const showNav = () => {
+      isShowing.value = !isShowing.value;
+    };
+
+    return { showNav, isShowing };
+  },
+};
+</script>
+
+<style></style>
