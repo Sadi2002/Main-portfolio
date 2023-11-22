15ba12b98f63ab46fdb46e4c26d7cf9c5d102278
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-11-22 08:32:12 +0100

    added about me section on tablet

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,37 +2,38 @@
   <div class="about-me">
     <span class="about-me-headline">About me</span>
     <h2 class="about-me-title">Passionate learner exploring web development</h2>
-
-    <div class="about-me-left-side">
-      <div class="about-me-left-side__box">
-        <img src="../assets/ambition.png" alt="bolt logo" />
-        <h3 class="about-me-left-side__title">My Ambition</h3>
-        <h3 class="about-me-left-side__description">
-          After work, I enthusiastically pursue self-learning in front-end
-          development. Despite not working in IT professionally, my evenings are
-          dedicated to expanding my knowledge and honing web development skills.
-          Motivated by a genuine passion for creating user-friendly digital
-          experiences, I strive to become a more proficient front-end enthusiast
-          with each passing day.
-        </h3>
+    <div class="both-sides">
+      <div class="about-me-left-side">
+        <div class="about-me-left-side__box">
+          <img src="../assets/ambition.png" alt="bolt logo" />
+          <h3 class="about-me-left-side__title">My Ambition</h3>
+          <h3 class="about-me-left-side__description">
+            After work, I enthusiastically pursue self-learning in front-end
+            development. Despite not working in IT professionally, my evenings
+            are dedicated to expanding my knowledge and honing web development
+            skills. Motivated by a genuine passion for creating user-friendly
+            digital experiences, I strive to become a more proficient front-end
+            enthusiast with each passing day.
+          </h3>
+        </div>
+        <div class="about-me-left-side__box">
+          <img src="../assets/purpose.png" alt="purpose logo" />
+          <h3 class="about-me-left-side__title">My Purpose</h3>
+          <h3 class="about-me-left-side__description">
+            Passionately pursuing the goal of becoming a top-tier front-end
+            developer, my commitment extends beyond regular working hours.
+            Despite not being professionally tied to IT, I invest my evenings in
+            intensive self-learning, aiming to deepen my knowledge and refine my
+            skills in web development. Motivated by an authentic ambition to
+            stand out in the field, every day is a stride toward achieving
+            excellence and becoming a highly skilled front-end enthusiast."
+          </h3>
+        </div>
       </div>
-      <div class="about-me-left-side__box">
-        <img src="../assets/purpose.png" alt="purpose logo" />
-        <h3 class="about-me-left-side__title">My Purpose</h3>
-        <h3 class="about-me-left-side__description">
-          Passionately pursuing the goal of becoming a top-tier front-end
-          developer, my commitment extends beyond regular working hours. Despite
-          not being professionally tied to IT, I invest my evenings in intensive
-          self-learning, aiming to deepen my knowledge and refine my skills in
-          web development. Motivated by an authentic ambition to stand out in
-          the field, every day is a stride toward achieving excellence and
-          becoming a highly skilled front-end enthusiast."
-        </h3>
+      <div class="about-me-right-side">
+        <img class="square" src="../assets/square.png" alt="square" />
+        <img class="me" src="../assets/me-on-bg.png" alt="myself" />
       </div>
-    </div>
-    <div class="about-me-right-side">
-      <img class="square" src="../assets/square.png" alt="square" />
-      <img class="me" src="../assets/me-on-bg.png" alt="myself" />
     </div>
   </div>
 </template>
