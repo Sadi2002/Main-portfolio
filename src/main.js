f95132895df1a60d86667e29c09cdaecabb89ad5
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-11-25 09:26:52 +0100

    added img height

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
+// main.js
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/global.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+
+// Zainicjuj AOS po zamontowaniu aplikacji
+app.component("AosInitialized", {
+  mounted() {
+    AOS.init();
+  },
+});
+
+app.component("AosRefresh", {
+  mounted() {
+    AOS.refresh();
+  },
+});
+
+app.mount("#app");
